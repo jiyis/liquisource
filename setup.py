b3055bc0972958a30eed9cb7d099ce8e47d2fabe
garypdong <garypdong@tencent.com>
2023-06-05 23:20:04 +0800

    init

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="liquisource",
+    version="1.0.0",
+    packages=find_packages(),
+    install_requires=[
+        "setuptools",
+        "jproperties",
+        "clickhouse_connect",
+        "pymongo"
+    ],
+    author="garypdong",
+    author_email="garypdong@tencent.com",
+    description="A package for liquibase monogo or clickhosue client.",
+    keywords="liquisource",
+    url="http://github.com/jiyis/liquisource"
+)
