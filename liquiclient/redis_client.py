f6187b2963a44245c679ffb47e1406a911c8416e
garypdong <garypdong@tencent.com>
2025-07-08 10:21:28 +0800

    支持redis cluster

--- liquiclient/redis_client.py
+++ liquiclient/redis_client.py
@@ -4,19 +4,20 @@
 import redis
 from liquiclient.config import get_property
 from redis.cluster import RedisCluster
+from urllib.parse import urlencode
 
 
 # 获取redis实例
 def get_redis_client():
     mode = get_property("redis.mode")
+    host = get_property("redis.host")
+    port = get_property("redis.port")
+    username = get_property("redis.username")
+    password = get_property("redis.password")
     if mode != "cluster":
-        host = get_property("redis.host")
-        port = get_property("redis.port")
-        username = get_property("redis.username")
-        password = get_property("redis.password")
         client = redis.Redis(host=host, port=port, username=username, password=password, decode_responses=True)
     else:
-        url = get_property("redis.host")
+        url = "redis://{}:{}@{}".format(urlencode(username), urlencode(password), host)
         client = RedisCluster.from_url(url, decode_responses=True)
 
     return client
@@ -25,15 +26,14 @@ def get_redis_client():
 # 获取redis实例
 def get_redis_cluster_client(key):
     mode = get_property("redis.mode")
+    host = get_property(key + ".redis.host")
+    port = get_property(key + ".redis.port")
+    username = get_property(key + ".redis.username")
+    password = get_property(key + ".redis.password")
     if mode != "cluster":
-        host = get_property(key + ".redis.host")
-        port = get_property(key + ".redis.port")
-        username = get_property(key + ".redis.username")
-        password = get_property(key + ".redis.password")
-
         client = redis.Redis(host=host, port=port, username=username, password=password, decode_responses=True)
     else:
-        url = get_property(key+"redis.host")
+        url = "redis://{}:{}@{}".format(urlencode(username), urlencode(password), host)
         client = RedisCluster.from_url(url, decode_responses=True)
 
     return client
