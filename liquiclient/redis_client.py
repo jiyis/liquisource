60e5611a56c583b71140fc928aea53eee848229c
garypdong <garypdong@tencent.com>
2025-07-16 11:05:08 +0800

    支持没有密码redis cluster

--- liquiclient/redis_client.py
+++ liquiclient/redis_client.py
@@ -14,10 +14,31 @@ def get_redis_client():
     port = get_property("redis.port")
     username = get_property("redis.username")
     password = get_property("redis.password")
+
     if mode != "cluster":
-        client = redis.Redis(host=host, port=port, username=username, password=password, decode_responses=True)
+        # 构建连接参数，只包含非空的认证信息
+        connection_params = {
+            'host': host,
+            'port': port,
+            'decode_responses': True
+        }
+
+        # 只有当username和password都不为空时才添加认证参数
+        if username and password:
+            connection_params['username'] = username
+            connection_params['password'] = password
+        elif password:  # 只有密码没有用户名的情况
+            connection_params['password'] = password
+
+        client = redis.Redis(**connection_params)
     else:
-        url = "redis://{}:{}@{}".format(urlencode(username), urlencode(password), host)
+        # 集群模式处理 - 修正URL构建，包含端口号
+        if username and password:
+            url = "redis://{}:{}@{}:{}".format(urlencode(username), urlencode(password), host, port)
+        elif password:
+            url = "redis://:{}@{}:{}".format(urlencode(password), host, port)
+        else:
+            url = "redis://{}:{}".format(host, port)
         client = RedisCluster.from_url(url, decode_responses=True)
 
     return client
@@ -30,10 +51,31 @@ def get_redis_cluster_client(key):
     port = get_property(key + ".redis.port")
     username = get_property(key + ".redis.username")
     password = get_property(key + ".redis.password")
+
     if mode != "cluster":
-        client = redis.Redis(host=host, port=port, username=username, password=password, decode_responses=True)
+        # 构建连接参数，只包含非空的认证信息
+        connection_params = {
+            'host': host,
+            'port': port,
+            'decode_responses': True
+        }
+
+        # 只有当username和password都不为空时才添加认证参数
+        if username and password:
+            connection_params['username'] = username
+            connection_params['password'] = password
+        elif password:  # 只有密码没有用户名的情况
+            connection_params['password'] = password
+
+        client = redis.Redis(**connection_params)
     else:
-        url = "redis://{}:{}@{}".format(urlencode(username), urlencode(password), host)
+        # 集群模式处理 - 修正URL构建，包含端口号
+        if username and password:
+            url = "redis://{}:{}@{}:{}".format(urlencode(username), urlencode(password), host, port)
+        elif password:
+            url = "redis://:{}@{}:{}".format(urlencode(password), host, port)
+        else:
+            url = "redis://{}:{}".format(host, port)
         client = RedisCluster.from_url(url, decode_responses=True)
 
     return client
